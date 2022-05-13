fd4bdf846e9fd1147858a5e3fe79ee4d28ad3ea2
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 17:06:14 +0530

    UC1-Database-Connection

---
+++ AddressBookRespitory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace ADO_Address_Book
+{
+    internal class AddressBookRespitory
+    {
+        //Give path for Database Connection
+        public static string connection = @"Server=LAPTOP-ABMNAC9K\SQLEXPRESS;Database=Address_Book_Service_DB;Trusted_Connection=True;";
+        //Represents a connection to Sql Server Database
+        SqlConnection sqlConnection = new SqlConnection(connection);
+    }
+}
